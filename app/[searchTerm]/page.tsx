ace761e9d371cd67703085b6d06878aee3f23a7f
selajdin89 <selajdinmemeti89@gmail.com>
2023-07-09 13:45:51 +0200

    Initial Commit

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,48 @@
+import getWikiResults from "@/lib/getWikiResults";
+import React from "react";
+import Item from "./components/Item";
+
+type Props = {
+  params: {
+    searchTerm: string;
+  };
+};
+
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+  const data = await wikiData;
+
+  const dispplayTerm = searchTerm.replaceAll("%20", " ");
+
+  if (!data?.query?.pages) {
+    return {
+      title: `${dispplayTerm} Not Found`,
+    };
+  }
+
+  return {
+    title: dispplayTerm,
+    description: `Search results for ${dispplayTerm}`,
+  };
+}
+
+export default async function page({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+  const data = await wikiData;
+
+  const results: Result[] | undefined = data?.query?.pages;
+
+  const content = (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+      {results ? (
+        Object.values(results).map((result) => (
+          <Item key={result.pageid} result={result} />
+        ))
+      ) : (
+        <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
+      )}
+    </main>
+  );
+
+  return content;
+}
